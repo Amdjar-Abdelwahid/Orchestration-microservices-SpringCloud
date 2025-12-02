9099a4e81506242c552584992b0c61507383cf84
Amdjar-Abdelwahid <amdjarabdelwahid1@gmail.com>
2025-12-02 20:54:49 +0100

    done

---
+++ GateWay/src/main/java/com/ma/GateWay/GateWayApplication.java
@@ -0,0 +1,39 @@
+package com.ma.GateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(GateWayApplication.class, args);
+	}
+
+    @Bean
+    RouteLocator staticRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/clients/**").uri("lb://SERVICE-CLIENT"))
+                .route(r -> r.path("/client/**").uri("lb://SERVICE-CLIENT"))
+                .route(r -> r.path("/voitures/**").uri("lb://SERVICE-VOITURE"))
+                .build();
+    }
+
+    
+    /*
+    @Bean
+    DiscoveryClientRouteDefinitionLocator routesDynamique(
+            ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
+
+     */
+
+}
